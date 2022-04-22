907cbddfc214369f350d34eb9678e5b66c3a04c3
AlenaVasileva <Alenadvasileva@yandex.ru>
2022-04-22 15:46:05 +0300

    recyclable view added

---
+++ app/src/main/java/com/example/wishgraphmobile/CustomAdapter.kt
@@ -0,0 +1,54 @@
+package com.example.wishgraphmobile
+
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.squareup.picasso.Picasso
+import org.json.JSONObject
+import java.io.FileNotFoundException
+
+class CustomAdapter(private val mList: List<String>) : RecyclerView.Adapter<CustomAdapter.ViewHolder>() {
+
+    // create new views
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        // inflates the card_view_design view
+        // that is used to hold list item
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.nft_layout, parent, false)
+
+        return ViewHolder(view)
+    }
+
+    // binds the list items to a view
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+
+        try {
+            val content = JSONObject(mList[position]).getJSONArray("content")
+            val picture_url = content.getJSONObject(0).getString("url")
+            Picasso.get()
+                .load(picture_url)
+                .into(holder.imageView);
+        } catch (ex: FileNotFoundException) {
+            Log.println(Log.DEBUG, Log.DEBUG.toString(), "CATCHED")
+        }
+
+        // sets the text to the textview from our itemHolder class
+        //holder.textView.text = "See"
+
+    }
+
+    // return the number of the items in the list
+    override fun getItemCount(): Int {
+        return mList.size
+    }
+
+    // Holds the views for adding it to image and text
+    class ViewHolder(ItemView: View) : RecyclerView.ViewHolder(ItemView) {
+        val imageView: ImageView = itemView.findViewById(R.id.imageview)
+        //val textView: TextView = itemView.findViewById(R.id.textView)
+    }
+}
--- app/src/main/java/com/example/wishgraphmobile/MainActivity.kt
+++ app/src/main/java/com/example/wishgraphmobile/MainActivity.kt
@@ -6,7 +6,10 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.*
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.squareup.picasso.Picasso
+import org.json.JSONArray
 import org.json.JSONObject
 import java.io.FileNotFoundException
 import java.net.HttpURLConnection
@@ -26,57 +29,65 @@ class MainActivity : AppCompatActivity() {
         wallet_field = findViewById(R.id.wallet_field)
         find_btn = findViewById(R.id.btn_find)
         result_info = findViewById(R.id.result)
-        image_view = findViewById(R.id.nft_view)
 
         find_btn?.setOnClickListener {
-            if(wallet_field?.text?.toString()?.trim()?.equals("")!!)
+            if (wallet_field?.text?.toString()?.trim()?.equals("")!!)
                 Toast.makeText(this, "Enter address <3", Toast.LENGTH_LONG).show()
             else {
+                result_info?.text = "Finding..."
                 val wallet: String = wallet_field?.text.toString()
                 val simpleRunnable = SimpleRunnable(wallet)
                 val threadWithRunnable = Thread(simpleRunnable)
                 threadWithRunnable.start()
-                result_info?.text="Finding..."
                 threadWithRunnable.join()
+                result_info?.text = "Recommended NFT"
                 val json = simpleRunnable.Text
-                val nft_array = JSONObject(json).getJSONArray("items")
-                val meta = nft_array.getJSONObject(0).getString("meta")
-                val content = JSONObject(meta).getJSONArray("content")
-                val picture_url = content.getJSONObject(0).getString("url")
-                Picasso.get()
-                    .load(picture_url)
-                    .into(image_view);
-                result_info?.text = picture_url
+                val nft_array: JSONArray = JSONObject(json).getJSONArray("items")
+                val recyclerview = findViewById<RecyclerView>(R.id.nft_view)
+
+                recyclerview.layoutManager = LinearLayoutManager(this)
+                val data = ArrayList<String>()
+                try {
+                    //for (i in 0..1) {
+                            data.add(nft_array.getJSONObject(0).getString("meta"))
+                            data.add(nft_array.getJSONObject(3).getString("meta"))
+                                    //data.add(nft_array.getJSONObject(6).getString("meta"))
+                    //}
+                } catch (ex: FileNotFoundException) {
+                    Log.println(Log.DEBUG, Log.DEBUG.toString(), "CATCHED")
+                }
+                val adapter = CustomAdapter(data)
+                recyclerview.adapter = adapter
             }
         }
     }
-}
 
-class SimpleRunnable(_wallet: String) : Runnable {
-    val Wallet: String
-    var Text: String = ""
+    class SimpleRunnable(_wallet: String) : Runnable {
+        val Wallet: String
+        var Text: String = ""
 
-    init {
-        Wallet = _wallet
-    }
-
-    public override fun run() {
-        val url =
-            "https://api.rarible.org/v0.1/items/byOwner?owner=ETHEREUM%3A$Wallet"
-        Log.println(Log.DEBUG, Log.DEBUG.toString(), url)
-        val connection = URL(url).openConnection() as HttpURLConnection
-        var text = "test"
-        try {
-            connection.connect()
-            text = connection.inputStream.use {
-                it.reader().use() { reader -> reader.readText() }
-            }
-            Text = text
-        } catch (ex: FileNotFoundException) {
-            Log.println(Log.DEBUG, Log.DEBUG.toString(), "CATCHED")
-        } finally {
-            connection.disconnect()
+        init {
+            Wallet = _wallet
         }
-        Log.println(Log.DEBUG, Log.DEBUG.toString(), "GET $text")
+
+        public override fun run() {
+            val url =
+                "https://api.rarible.org/v0.1/items/byOwner?owner=ETHEREUM%3A$Wallet"
+            Log.println(Log.DEBUG, Log.DEBUG.toString(), url)
+            val connection = URL(url).openConnection() as HttpURLConnection
+            var text = "test"
+            try {
+                connection.connect()
+                text = connection.inputStream.use {
+                    it.reader().use() { reader -> reader.readText() }
+                }
+                Text = text
+            } catch (ex: FileNotFoundException) {
+                Log.println(Log.DEBUG, Log.DEBUG.toString(), "CATCHED")
+            } finally {
+                connection.disconnect()
+            }
+            Log.println(Log.DEBUG, Log.DEBUG.toString(), "GET $text")
+        }
     }
 }
